2155cfc301c9c64f8c621795572dac0e8423ed82
i'mfebri <iamfebriadji@gmail.com>
2021-02-08 11:54:34 +0700

    update db.js

--- models/db.js
+++ models/db.js
@@ -15,6 +15,29 @@ db.connect(err => {
   db.query('CREATE DATABASE IF NOT EXISTS restapi', (err, result) => {
     if (err) return console.log(err)
   })
+
+  let listAnime = `CREATE TABLE IF NOT EXISTS listAnime (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    author VARCHAR(225) DEFAULT '-',
+    title VARCHAR(225) NOT NULL,
+    genre VARCHAR(225),
+    status ENUM('Ongoing', 'Completed'),
+    rating FLOAT(3,2) DEFAULT 0.00
+  )`
+  
+  db.query(listAnime, (err, result) => {
+    if (err) console.log(err)
+  })
+
+  let descAnime = `CREATE TABLE IF NOT EXISTS descAnime (
+    description TEXT DEFAULT '-',
+    idList INT,
+    FOREIGN KEY (idList) REFERENCES listAnime(id)
+  )`
+
+  db.query(descAnime, (err, result) => {
+    if (err) console.log(err)
+  })
 })
 
 module.exports = db
