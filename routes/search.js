427f33bd771582e684ad2997529878a0340f9c42
i'mfebri <iamfebriadji@gmail.com>
2021-02-08 23:46:22 +0700

    develop seacrh.js

--- routes/search.js
+++ routes/search.js
@@ -1,12 +1,15 @@
 "use strict"
+const { json } = require('body-parser')
 const db = require('../models/db')
 
 module.exports = (req, res, next) => {
   let uriTitle = req.query.title
   let uriGenre = req.query.genre
+  let uriRating = req.query.rating
 
-  if ( uriTitle == undefined && uriGenre == undefined ) {
-    let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList`
+  if ( uriTitle ) {
+    let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList WHERE title LIKE '%${uriTitle}%'`
+
     db.query(sql, (err, result) => {
       if (err) return res.json({ message: err })
 
@@ -14,9 +17,35 @@ module.exports = (req, res, next) => {
     })
   }
 
-  if ( uriTitle ) {
-    let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList WHERE title LIKE '%${uriTitle}%'`
+  if ( uriGenre ) {
+    let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList`
+    db.query(sql, (err, result) => {
+      if (err) return res.json({ message: err })
+      
+      result.map( list => {
+        const genreList = list.genre.split(", ")
 
+        for ( let i in genreList ) {
+          if ( genreList[i].toLowerCase() == uriGenre ) {
+            res.json({ list: list })
+          }
+        }
+      })
+    })
+  }
+
+  if ( uriRating ) {
+    let sql = `SELECT * FROM listAnime WHERE rating < ${uriRating}`
+
+    db.query(sql, (err, result) => {
+      if (err) return res.json({ message: err })
+
+      res.json({ list: result })
+    })
+  }
+
+  if ( uriTitle == undefined && uriGenre == undefined && uriRating == undefined ) {
+    let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList ORDER BY listAnime.title ASC`
     db.query(sql, (err, result) => {
       if (err) return res.json({ message: err })
 
