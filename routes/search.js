545ef6a722b183a30cd6869cf8ec14f92a3be9bc
i'mfebri <iamfebriadji@gmail.com>
2021-02-09 13:31:41 +0700

    update search.js

--- routes/search.js
+++ routes/search.js
@@ -1,13 +1,22 @@
 "use strict"
-const { json } = require('body-parser')
 const db = require('../models/db')
 
 module.exports = (req, res, next) => {
+  let uriId = req.query.id
   let uriTitle = req.query.title
   let uriGenre = req.query.genre
   let uriRating = req.query.rating
 
-  if ( uriTitle ) {
+  if ( uriId ) {
+    let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList WHERE id = ?`
+
+    db.query(sql, [uriId], (err, result) => {
+      if (err) return res.json({ message: err })
+
+      res.json({ list: result })
+    })
+  
+  } else if ( uriTitle ) {
     let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList WHERE title LIKE '%${uriTitle}%'`
 
     db.query(sql, (err, result) => {
@@ -15,37 +24,37 @@ module.exports = (req, res, next) => {
 
       res.json({ list: result })
     })
-  }
 
-  if ( uriGenre ) {
+  } else if ( uriGenre ) {
     let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList`
+    
     db.query(sql, (err, result) => {
       if (err) return res.json({ message: err })
       
       result.map( list => {
         const genreList = list.genre.split(", ")
-
+        
         for ( let i in genreList ) {
           if ( genreList[i].toLowerCase() == uriGenre ) {
-            res.json({ list: list })
+            console.log(list)
+            res.end()
           }
         }
       })
     })
-  }
 
-  if ( uriRating ) {
-    let sql = `SELECT * FROM listAnime WHERE rating < ${uriRating}`
+  } else if ( uriRating ) {
+    let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idlist WHERE rating <= ${uriRating}`
 
     db.query(sql, (err, result) => {
       if (err) return res.json({ message: err })
 
       res.json({ list: result })
     })
-  }
-
-  if ( uriTitle == undefined && uriGenre == undefined && uriRating == undefined ) {
+    
+  } else {
     let sql = `SELECT * FROM listAnime LEFT JOIN descAnime ON listAnime.id = descAnime.idList ORDER BY listAnime.title ASC`
+    
     db.query(sql, (err, result) => {
       if (err) return res.json({ message: err })
 
